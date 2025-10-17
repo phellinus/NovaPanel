808a9426a89f12195348a9a13b50f2886e07c981
桑榆 <2249736997@qq.com>
2025-10-17 22:13:20 +0800

    feat:tailwindcss的引入，登录页面初始化

--- src/views/welcome/index.tsx
+++ src/views/welcome/index.tsx
@@ -1,7 +1,32 @@
+import api from '../../utils/request.ts';
+
+console.log(api);
 const Welcome = () => {
+    const handleStorage = (type: string) => {
+        switch (type) {
+            case 'get':
+                console.log(localStorage.getItem('tokenReact'));
+                break;
+            case 'set':
+                localStorage.setItem('tokenReact', '123456');
+                break;
+            case 'remove':
+                localStorage.removeItem('tokenReact');
+                break;
+            case 'clear':
+                localStorage.clear();
+                break;
+            default:
+                break;
+        }
+    };
     return (
         <>
             <h1>欢迎来到Nova Panel</h1>
+            <button onClick={() => handleStorage('get')}>获取</button>
+            <button onClick={() => handleStorage('set')}>设置</button>
+            <button onClick={() => handleStorage('remove')}>删除</button>
+            <button onClick={() => handleStorage('clear')}>清空</button>
         </>
     );
 };
