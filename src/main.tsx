197180dbc48ec4908eb51fcbe80d9d4f657af5ee
桑榆 <2249736997@qq.com>
2025-10-20 23:57:00 +0800

    feat:路由修改

--- src/main.tsx
+++ src/main.tsx
@@ -2,8 +2,6 @@ import { createRoot } from 'react-dom/client';
 import '@ant-design/v5-patch-for-react-19';
 import './index.css';
 import '@/styles/theme.css';
+import App from './App';
 
-import { RouterProvider } from 'react-router-dom';
-import router from './router';
-
-createRoot(document.getElementById('root')!).render(<RouterProvider router={router} />);
+createRoot(document.getElementById('root')!).render(<App />);
--- src/router/index.tsx
+++ src/router/index.tsx
@@ -2,19 +2,14 @@ import { createBrowserRouter } from 'react-router-dom';
 import LoginPage from '../views/login';
 import HomePage from '../views/home';
 import Welcome from '../views/welcome';
-import App from '../App';
 
 const router = createBrowserRouter([
-    {
-        path: '/',
-        element: <App />,
-    },
     {
         path: '/login',
         element: <LoginPage />,
     },
     {
-        path: '/welcome',
+        path: '/',
         element: <Welcome />,
     },
     {
